b9ea9c605c1860ee2bad4141a605e5621ff56684
putricahya <putricahya868@gmail.com>
2018-10-05 07:53:59 +0700

    initiate project

---
+++ app/src/main/java/com/example/amikom/singleaplication/Second_activity.java
@@ -0,0 +1,13 @@
+package com.example.amikom.singleaplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_activity);
+    }
+}
